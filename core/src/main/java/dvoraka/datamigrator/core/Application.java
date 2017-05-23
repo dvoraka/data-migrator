56f1929a1ad3c6f561e5dadf851a4fb61c43ee79
Alen Dvorak <alen.dvorak@gmail.com>
2017-05-23 17:35:05 +0200

    Prepare modules

---
+++ core/src/main/java/dvoraka/datamigrator/core/Application.java
@@ -0,0 +1,11 @@
+package dvoraka.datamigrator.core;
+
+/**
+ * Application.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Application");
+    }
+}
